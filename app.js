1065a8c8232ab66107eddd3ea822a43d244b8ca5
rashidtvmr <rashidtvmr@gmail.com>
2021-07-27 12:06:45 +0530

    sd

--- app.js
+++ app.js
@@ -1,44 +1,68 @@
-const express = require('express');
+// const puppeteer = require('puppeteer');
+const port = process.env.PORT || 8116;
+const express = require("express");
 const app = express();
-const puppeteer = require('puppeteer');
-const port = process.env.PORT || 8080;
-const validUrl = require('valid-url');
+const __puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+// const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha')
 
-var parseUrl = function(url) {
-    url = decodeURIComponent(url)
-    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
-        url = 'http://' + url;
-    }
+puppeteer.use(StealthPlugin());
+// puppeteer.use(RecaptchaPlugin())
+runTasks();
+runTasks();
+runTasks();
+runTasks();
 
-    return url;
-};
+setInterval(() => {
+    console.log('looping started', new Date().toLocaleString());
+    runTasks();
+    runTasks();
+    runTasks();
+    runTasks();
+}, 10 * 60 * 1000);
 
-app.get('/', function(req, res) {
-    var urlToScreenshot = parseUrl(req.query.url);
+async function runTasks() {
+    console.log("running task browser", new Date().toLocaleString());
+    let browser = await puppeteer.launch({
+        // headless: true,
+        headless: false,
+        // args: ['--proxy-server=socks5://127.0.0.1:9050']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+        ],
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.youtube.com/watch?v=F-lK1N0VWyk&list=PLGxDMC0jMjTf2ixuiGEhJGCA0rVW3bGp-');
+    await page.waitForSelector('#movie_player');
+    await page.evaluate(() => {
+        // let isModalCont = document.evaluate('/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[2]/div[2]/div[5]/div[2]/ytd-button-renderer[2]/a/tp-yt-paper-button', document, null, XPathResult.ANY_TYPE, null)
+        // let isModal = isModalCont.iterateNext();
+        // if (Boolean(isModal));
+        //     isModal.click();
+        const videoController = document.getElementsByTagName('video')[0];
+        const videoElmnt = document.getElementById('movie_player');
+        const loopButton = document.querySelectorAll('button[aria-label="Loop playlist"]');
+        if (loopButton.length > 0)
+            loopButton[0].click();
 
-    if (validUrl.isWebUri(urlToScreenshot)) {
-        console.log('Screenshotting: ' + urlToScreenshot);
-        (async() => {
-            const browser = await puppeteer.launch({
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
-            });
+        // if (videoController)
+        //     videoController.loop = true;
+        if (videoElmnt) videoElmnt.playVideo();
+    })
+    setInterval(async () => {
+        console.log('taking screenshot every minutes..', new Date().toLocaleString());
+        await page.screenshot({ path: `./${new Date().toLocaleTimeString()}.png` })
+    }, 8 * 60 * 1000)
+    setTimeout(async () => {
+        console.log('killing browser..', new Date().toLocaleString());
+        await browser.close();
+    }, 9 * 60 * 1000)
+}
 
-            const page = await browser.newPage();
-            await page.goto(urlToScreenshot);
-            await page.screenshot().then(function(buffer) {
-                res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
-                res.setHeader('Content-Type', 'image/png');
-                res.send(buffer)
-            });
 
-            await browser.close();
-        })();
-    } else {
-        res.send('Invalid url: ' + urlToScreenshot);
-    }
-
+app.get('/', (req, res) => res.json({ status: 200, statusText: 'Running' }))
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
 });
-
-app.listen(port, function() {
-    console.log('App listening on port ' + port)
-})
